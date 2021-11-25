9e5d75d1158afe6266418cfc2966b6657eb91b0c
Shampad <sompodsign@gmail.com>
2021-11-25 22:06:53 +0600

    delegate iternation problem

--- deletage_iteration.py
+++ deletage_iteration.py
@@ -4,13 +4,14 @@ class Node:
         self._children = []
 
     def __repr__(self):
-        return 'Node({!r})'.format(self._value)
+        return 'Node({})'.format(self._value)
 
     def add_child(self, node):
         self._children.append(node)
 
     def __iter__(self):
         return iter(self._children)
+
 
 if __name__ == '__main__':
     root = Node(0)
