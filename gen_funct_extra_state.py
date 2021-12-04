8f8be6aa425173f475613bae68159fb5ca4dcba9
shampad <email@example.com>
2021-12-04 17:24:22 +0600

    generator extra state

---
+++ gen_funct_extra_state.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+
+class LineHistory:
+    def __init__(self, lines, histlen=3):
+        self.lines = lines
+        self.history = deque(maxlen=histlen)
+
+    def __iter__(self):
+        for lineno, line in enumerate(self.lines, 1):
+            self.history.append((lineno, line))
+            yield line
+
+    def clear(self):
+        self.history.clear()
+
+
+with open("somefile.txt") as f:
+    lines = LineHistory(f)
+    for line in lines:
+        if 'python' in line:
+            for lineno, hline in lines.history:
+                print('{}:{}'.format(lineno, hline), end='')
