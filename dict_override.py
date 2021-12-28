c36a5d80d03c4fb095234899474b07d2bdc08eab
sompodsign <sompodsign@gmail.com>
2021-12-28 09:25:07 +0600

    mro

--- dict_override.py
+++ dict_override.py
@@ -3,8 +3,13 @@ class DoppelDict(dict):
         super().__setitem__(key, value * 2)
 
 
-if __name__ == '__main__':
-    d = DoppelDict(one=1)
-    print(d)
-    d['two'] = 2
-    print(d)
+import collections
+
+class DoppelDict(collections.UserDict):
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value * 4)
+
+d = DoppelDict({"one": 1})
+print(d)
+d1 = DoppelDict({"two": 2})
+print(d1)
